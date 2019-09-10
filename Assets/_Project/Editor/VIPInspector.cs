3ada700646daac53484cf2327e7b2c6eddc7fdc3
Vivi Ma <vinjiminster@gmail.com>
2019-09-10 15:30:41 -0400

    VIP movement

---
+++ Assets/_Project/Editor/VIPInspector.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(VIP))]
+public class VIPInspector : Editor
+{
+    private VIP vip;
+    private void OnSceneGUI()
+    {
+        vip = target as VIP;
+        Handles.color = Color.green;
+        Handles.DrawLine(vip.transform.position, vip.currentDest);
+        Handles.DrawWireDisc(vip.target.transform.position, new Vector3(0, 0, 1), vip.maxRadius);
+        Handles.DrawWireDisc(vip.target.transform.position, new Vector3(0, 0, 1), vip.minRadius);
+    }
+}
